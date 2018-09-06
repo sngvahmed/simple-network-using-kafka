112e85dfc62343a8a853a8805347755d4e778942
Ahmed Nasser <anasser.ext@orange.com>
2018-09-06 21:22:24 +0200

    update paths

--- generate.sh
+++ generate.sh
@@ -1,10 +1,13 @@
-rm channel-artifacts/*
 rm -r crypto-config
+rm -r channel-artifacts
 
-cryptogen generate --config=crypto-config.yaml —-output=crypto-config
+mkdir crypto-config
+mkdir channel-artifacts
+cryptogen generate --config=crypto-config.yaml --output=crypto-config
 
 configtxgen -profile OrgsChannel -outputCreateChannelTx channel.tx -channelID mychannel
-configtxgen -profile OrgsChannel  -outputAnchorPeersUpdate Org1MSPanchors.tx -channelID mychannel -asOrg Org1MSP
+configtxgen -profile OrgsChannel  -outputAnchorPeersUpdate Org1MSPanchors.tx -channelID mychannel -asOrg org1MSP
+configtxgen -profile OrgsChannel  -outputAnchorPeersUpdate Org2MSPanchors.tx -channelID mychannel -asOrg org2MSP
 configtxgen -profile OrgsOrdererGenesis -outputBlock genesis.block
 
 mv genesis.block channel.tx Org1MSPanchors.tx channel-artifacts/
